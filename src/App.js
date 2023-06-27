4ce9935fb32bbb8cf2ee5c4383fd371702de8433
Wes <claude.w.fountain@gmail.com>
2023-06-27 12:55:56 -0400

    major refactor changed data flow

--- src/App.js
+++ src/App.js
@@ -1,59 +1,12 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState, useEffect } from 'react';
-import ApiCallCreator from './components/ApiCallCreator';
-import FetchMovieData from './components/FetchMovieData';
 
 function App() {
-  const [searchObj, setSearchObj] = useState({})
-  const [selectedGenre, setSelectedGenre] = useState(null);
-  const [selectedDecade, setSelectedDecade] = useState(null);
-  const [searchInput, setSearchInput] = useState(null);
-  const [searchPopupMoives, setSearchPopupMoives] = useState(null) 
-  const [apiCall, setApiCall] = useState();
-
-  const handleFilterChange = (filter) => {
-    if(filter.genre !== null && filter.genre !== undefined){
-      setSelectedGenre(filter.genre);
-    }
-    if(filter.decade !== null && filter.decade !== undefined){
-      setSelectedDecade(filter.decade);
-    }
-  };
-
-  useEffect(() => {
-    if(selectedDecade === null){
-      setSelectedDecade('All')
-    }
-    setSearchObj({
-      genre: selectedGenre,
-      decade: selectedDecade,
-      query: searchInput,
-  });
-  }, [selectedGenre, selectedDecade, searchInput])
-
-  const apiCallProp = (value) => {
-    setApiCall(value);
-  };
-
-  const handleSearchChange = (value) =>{
-    setSearchInput(value)
-  }
-
-  const handleMovieData = (value) => {
-    setSearchPopupMoives(value)
-  }
 
   return (
     <>
-      <NavBar
-        searchPopupMoives={searchPopupMoives}
-        onSearchChange={handleSearchChange}
-        onFilterChange={handleFilterChange}
-      />
-        <FetchMovieData fetchedMovieData={handleMovieData} searchInput={searchInput} searchObj={searchObj} finishedApiUrl={apiCall}/>
-        <ApiCallCreator searchObj={searchObj} apiCallProp={apiCallProp}/>
+      <NavBar />
     </>
   );
 }
--- src/components/DisplayMovies.js
+++ src/components/DisplayMovies.js
@@ -7,6 +7,7 @@ function DisplayMovies({movieObjects, searchObj}) {
   const [myMoviesObject, setMyMoviesObject] = useState();
   const [numberOfMoviesDisplayed, setNumberOfMoviesDisplayed] = useState();
   const [screenWidth, setScreenWidth] = useState(0);
+  const [seletedGenre, setSelectedGenre] = useState()
 
   useEffect(() => {
     if(movieObjects.length > 0){
@@ -15,21 +16,25 @@ function DisplayMovies({movieObjects, searchObj}) {
   },[movieObjects]);
 
   useEffect(()=>{
-    if(myMoviesObject !== undefined){
-      scrollToGenre()
+    if(searchObj.genre !== undefined && searchObj.genre !== null){
+      setSelectedGenre(searchObj.genre)
     }
-  },[searchObj.genre])
+  })
+  
+  useEffect(()=>{
+    if(seletedGenre !== undefined){
+      scrollToGenre(seletedGenre)
+    }
+  },[seletedGenre])
 
   useEffect(() => {
     const handleResize = () => {
       const width = window.innerWidth;
       setScreenWidth(width);
     };
-    // Add an event listener to track window resize
     window.addEventListener('resize', handleResize);
-    // Get the initial screen size on component mount
+
     handleResize();
-    // Cleanup the event listener when the component unmounts
     return () => {
       window.removeEventListener('resize', handleResize);
     };
@@ -39,15 +44,14 @@ function DisplayMovies({movieObjects, searchObj}) {
     // Perform actions based on screen size
     if (screenWidth > 768) {
       setNumberOfMoviesDisplayed(5)
-      // Do something when the screen size is larger than 768px
     } else {
       setNumberOfMoviesDisplayed(2)
-      // Do something else when the screen size is smaller than or equal to 768px
     }
   }, [screenWidth]);
 
-  const scrollToGenre = () => {
-    const genreElement = document.getElementById(searchObj.genre.name.toLowerCase());
+  const scrollToGenre = (name) => {
+    name = name.toLowerCase()
+    const genreElement = document.querySelector(`#${name}`);
     genreElement.scrollIntoView({ behavior: 'smooth', block: 'center'  });
   };
 
@@ -87,7 +91,7 @@ function DisplayMovies({movieObjects, searchObj}) {
                       {myGenre.movieList.map(function(movie, index) {
                          if (index >= activeIndex && index < activeIndex + numberOfMoviesDisplayed) {
                         return(
-                          <div className='my-movies'id={categoryTitle.toLowerCase()} style={{ width: '100%'}}   key={movie.id}>
+                          <div className='my-movies'id={props.genreName.toLowerCase()} style={{ width: '100%'}}   key={movie.id}>
                             <li key={movie.id}>
                               <MoviePopover props={movie} moviePoster={movie.poster_path} />
                             </li>
